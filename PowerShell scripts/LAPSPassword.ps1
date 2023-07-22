bb7fd905ca1acc8ef3be29786f36d1d7b96d0629
UrbiLab <90254842+UrbiLab@users.noreply.github.com>
2023-07-22 23:10:06 +0200

    Create LAPSPassword.ps1

---
+++ PowerShell scripts/LAPSPassword.ps1
@@ -0,0 +1,15 @@
+Install-Module microsoft.graph -Scope AllUsers
+
+Connect-MgGraph -Scope "Device.Read.All","DeviceLocalCredential.Read.All"
+
+$TenantID = (Get-MgContext).TenantId 
+$ApplicationID = "xxxxxxx" 
+Connect-MgGraph -TenantID $TenantID -ClientID $ApplicationID
+
+$NameDevice = "xyz"
+Get-LapsAADPassword -DeviceIds $NameDevice -IncludePasswords -AsPlainText -IncludeHistory
+
+$deviceDetails = Get-MgDevice -Search "displayName:$devName" -ConsistencyLevel eventual
+$answer = invoke-MgGraphRequest -Method Get https://graph.microsoft.com/beta/deviceLocalCredentials/$($deviceDetails.DeviceId)?`$select=credentials
+$andswer.credentials[0]
+[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($response.credentials[0].passwordBase64))
