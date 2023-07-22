23c38a59b8ffe75bc98274dc5d571430e9d295d4
UrbiLab <90254842+UrbiLab@users.noreply.github.com>
2023-07-22 23:10:42 +0200

    Update LAPSPassword.ps1

--- PowerShell scripts/LAPSPassword.ps1
+++ PowerShell scripts/LAPSPassword.ps1
@@ -9,7 +9,7 @@ Connect-MgGraph -TenantID $TenantID -ClientID $ApplicationID
 $NameDevice = "xyz"
 Get-LapsAADPassword -DeviceIds $NameDevice -IncludePasswords -AsPlainText -IncludeHistory
 
-$deviceDetails = Get-MgDevice -Search "displayName:$devName" -ConsistencyLevel eventual
+$deviceDetails = Get-MgDevice -Search "displayName:$NameDevice" -ConsistencyLevel eventual
 $answer = invoke-MgGraphRequest -Method Get https://graph.microsoft.com/beta/deviceLocalCredentials/$($deviceDetails.DeviceId)?`$select=credentials
 $andswer.credentials[0]
 [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($response.credentials[0].passwordBase64))
