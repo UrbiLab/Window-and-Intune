5f6554eb21aecbc1525f74bec2de74409b3d0740
UrbiLab <90254842+UrbiLab@users.noreply.github.com>
2023-07-25 21:02:23 +0200

    Delete checkup.ps1

--- PowerShell scripts/checkup.ps1
+++
@@ -1,8 +0,0 @@
-$PackageName = "Wallpaper"
-$Version = 1
-
-$ProgramVersion_current = Get-Content -Path "C:\ProgramData\urbilab\Validation\$PackageName" 
-
-if($ProgramVersion_current -eq $Version){
-    Write-Host "Found it!"
-}
